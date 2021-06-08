c8570170929a60d0323afc96138a6321198087c1
Anya <annagapyuk@gmail.com>
2021-06-08 18:57:45 -0400

    quotes are being fetched from API

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 // Get Quotes from API
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  quoteText.textContent = quote.text;
+  // check if author field is blank and replace it with unknown
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length to determine stylings
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  authorText.textContent = quote.author;
+}
 
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
   } catch (error) {
     // Catch error here
   }
 }
+
+// linkedin post
+
+function twitterQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", twitterQuote);
+
+// On Load
+getQuotes();
